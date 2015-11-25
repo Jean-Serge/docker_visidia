2b908d398dca9f9afe887a0d35bde6e18b6529df
Jean-Serge Monbailly <j.monbailly@orange.fr>
2015-11-25 15:43:40 +0100

    A améliorer

--- visidia.bash
+++ visidia.bash
@@ -4,4 +4,4 @@
 mkdir ./workspace 2>/dev/null
 
 docker build -t docker_visidia .
-docker run -ti --rm --name visidia -e DISPLAY -v $HOME/.Xauthority:/home/visidia/workspace/.Xauthority -v `pwd`/workspace:/home/visidia/workspace --net=host docker_visidia
+docker run -ti --rm --name visidia -e DISPLAY=$DISPLAY -v $HOME/.Xauthority:/home/visidia/workspace/.Xauthority -v `pwd`/workspace:/home/visidia/workspace --net=host docker_visidia
